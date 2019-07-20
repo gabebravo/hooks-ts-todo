214c2bba4e018db88dd027ba2397a748aa883dcb
Gabe Bravo <gabecbravo@gmail.com>
2019-07-19 20:34:00 -0400

    Got the setter to work from inside useEffect

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Grid, Typography, List } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { GlobalContext } from '../context';
-import { useFetch } from '../hooks/index';
 import { TodoProps } from '../types';
 import Todo from './Todo';
 
@@ -23,10 +22,16 @@ const useStyles = makeStyles(theme => ({
 export default function Todos() {
   const globalReducer = React.useContext(GlobalContext);
   const { state, dispatch } = globalReducer;
-  
-  const todos = useFetch('https://jsonplaceholder.typicode.com/todos', []);
+  const { todos } = state;
   const classes = useStyles();
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(res => res.json())
+      .then(json => {
+        dispatch({ type: 'SET_TODOS', payload: json });
+      });
+  }, []);
 
   console.log('state', state);
 
