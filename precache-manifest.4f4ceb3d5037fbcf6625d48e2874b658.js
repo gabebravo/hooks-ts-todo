4bba323b0133507ab250f662bad3cc66f4e1adfe
Gabe Bravo <gabecbravo@gmail.com>
2019-07-20 21:38:09 -0400

    Updates

---
+++ precache-manifest.4f4ceb3d5037fbcf6625d48e2874b658.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b71dbe1967ffa74ceadaa9aa53f5609d",
+    "url": "/gabebravo/hooks-ts-todo/index.html"
+  },
+  {
+    "revision": "4f79cb99400453c6ae7d",
+    "url": "/gabebravo/hooks-ts-todo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5e53a62cd18ed45ccb4e",
+    "url": "/gabebravo/hooks-ts-todo/static/js/2.88650e86.chunk.js"
+  },
+  {
+    "revision": "4f79cb99400453c6ae7d",
+    "url": "/gabebravo/hooks-ts-todo/static/js/main.d7e4d494.chunk.js"
+  },
+  {
+    "revision": "44200a120a08ab30f7e0",
+    "url": "/gabebravo/hooks-ts-todo/static/js/runtime~main.2eda7bdb.js"
+  }
+]);
